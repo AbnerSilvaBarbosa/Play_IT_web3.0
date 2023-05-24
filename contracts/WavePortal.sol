0f1a3f829b562bf840d2c18e69be34b66e40f5b3
AbnerSilvaBarbosa <abner.barbosa@sou.inteli.edul.br>
2023-05-23 23:03:57 -0300

    FEAT: random number contract and some ajusts

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -8,6 +8,7 @@ contract WavePortal {
 
     uint256 totalWaves;
     Wave[] waves;
+    uint256 private seed;
 
     event NewWave(address indexed from, uint256 timestamp, string message);
 
@@ -17,26 +18,46 @@ contract WavePortal {
         uint256 timestamp; // Data/hora de quando o usuário tchauzinhou.
     }
 
+    mapping(address => uint256) public lastWavedAt;
+
 
     constructor() payable{
         console.log("eu fui deployado !!!");
+         seed = (block.timestamp + block.difficulty) % 100;
     }
 
     function wave(string memory _message) public{
+
+        require(
+            lastWavedAt[msg.sender] + 15 minutes < block.timestamp,
+            "Espere 15m"
+        );
+
+        lastWavedAt[msg.sender] = block.timestamp;
+
         totalWaves += 1;
         console.log("%s deu tchauzinho!", msg.sender, _message);
 
         waves.push(Wave(msg.sender, _message, block.timestamp));
 
+        seed = (block.difficulty + block.timestamp + seed) % 100;
+
+        console.log("# randomico gerado: %d", seed);
+
+        if (seed <= 50) {
+            console.log("%s ganhou!", msg.sender);
+
+         
+            uint256 prizeAmount = 0.0001 ether;
+            require(
+                prizeAmount <= address(this).balance,
+                "Tentando sacar mais dinheiro que o contrato possui."
+            );
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Falhou em sacar dinheiro do contrato.");
+        }
+
         emit NewWave(msg.sender, block.timestamp, _message);
-
-        uint256 prizeAmount = 0.0001 ether;
-
-        require( prizeAmount <= address(this).balance, "Tentando sacar mais dinheiro que o contrato possui.");
-
-        (bool success, ) = (msg.sender).call{value: prizeAmount}("");
-
-        require(success, "Falhou em sacar dinheiro do contrato.");
     }
 
     function getAllWaves() public view returns (Wave[] memory) {
